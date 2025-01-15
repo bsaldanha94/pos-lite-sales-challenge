90b1fffe0add3beec2ec3c2e00c16df8db06435c
bsaldanha94 <60551060+bsaldanha94@users.noreply.github.com>
2025-01-15 12:46:41 +0530

    Create sales_funnel_kpis.sql

---
+++ data_mart_sqls/sales_funnel_kpis.sql
@@ -0,0 +1,20 @@
+SELECT
+  date
+, currency
+, country
+, campaign_id
+, campaign_name
+, product 
+, channel_3
+, channel_4
+, channel_5 
+, total_impressions
+, total_clicks
+, total_spends
+, total_leads
+, total_fake_leads
+, total_sql
+, total_meeting_done
+, total_signed_leads
+, total_pos_lite_deals
+FROM staging_sales_funnel_data
